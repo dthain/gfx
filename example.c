d322c99779b0e9c65da32f2ee23defbffea19b8a
Douglas Thain <dthain@nd.edu>
2024-02-08 15:18:13 -0500

    Put redraw in loop so you get it back after moving window.

--- example.c
+++ example.c
@@ -18,17 +18,20 @@ int main()
 	// Set the current drawing color to green.
 	gfx_color(0,200,100);
 
-	// Draw a triangle on the screen.
-	gfx_line(100,100,200,100);
-	gfx_line(200,100,150,150);
-	gfx_line(150,150,100,100);
-
 	while(1) {
+
+		// Draw a triangle on the screen.
+		gfx_line(100,100,200,100);
+		gfx_line(200,100,150,150);
+		gfx_line(150,150,100,100);
+
 		// Wait for the user to press a character.
 		c = gfx_wait();
 
 		// Quit if it is the letter q.
 		if(c=='q') break;
+
+		// Go back around and draw again, in case the window was moved.
 	}
 
 	return 0;
